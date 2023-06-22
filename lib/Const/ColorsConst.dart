d82708d655b46948ce2724031a577d72877ffe4c
xhambluefox <inforachid.rb@gmail.com>
2023-06-22 23:17:46 +0100

    Add Const Colors

---
+++ lib/Const/ColorsConst.dart
@@ -0,0 +1,10 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const  cardColor1 = Color(0xFFf2a48f);
+const  cardyellewColor = Color(0xFFfefaa0);
+const  cardColor2 = Color(0xFFf8e7b4);
+const  cardColor3 = Color(0xFFcdeff1);
+const  buttonColor = Color(0xFF080808);
