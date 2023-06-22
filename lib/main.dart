18bb77a78dd369d6ac384b7414e18903ff8a2a82
xhambluefox <inforachid.rb@gmail.com>
2023-06-22 22:06:09 +0100

    Change ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+
+
       theme: ThemeData(
+        brightness: Brightness.dark,
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
