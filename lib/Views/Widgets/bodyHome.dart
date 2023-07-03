bde1851a44e12f6a7046bfd767cdf76c41a1e2de
xhambluefox <inforachid.rb@gmail.com>
2023-07-03 22:51:17 +0100

    Add MansouryGridview

--- lib/Views/Widgets/bodyHome.dart
+++ lib/Views/Widgets/bodyHome.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import 'package:notes_app/Views/Widgets/CardWidgets.dart';
+import 'package:notes_app/Views/Widgets/MansouryGridView.dart';
 import 'package:notes_app/Views/Widgets/appBarWidgets.dart';
 
 class BodyHomePage extends StatefulWidget {
@@ -46,14 +47,4 @@ class _BodyHomePageState extends State<BodyHomePage> {
   }
 }
 
-Widget mansouryGridView(){
-  return MasonryGridView.builder(
-    scrollDirection: Axis.vertical,
-    physics: BouncingScrollPhysics(),
-    itemCount: 10,
-      gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (context, index){
 
-    return CardWidgets();
-      });
-}
