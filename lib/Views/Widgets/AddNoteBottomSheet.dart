d6c011789c8e4971ce7f1d5d6159fd39d6c20aca
xhambluefox <inforachid.rb@gmail.com>
2023-07-09 22:01:28 +0100

    Add TextFieldsto bottomSheet

--- lib/Views/Widgets/AddNoteBottomSheet.dart
+++ lib/Views/Widgets/AddNoteBottomSheet.dart
@@ -1,12 +1,23 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
-import 'package:notes_app/Const/ColorsConst.dart';
+import 'package:notes_app/Views/Widgets/CustomTextField.dart';
+
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return  Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children:  [
+          SizedBox(height: 34,),
+          CustomTextField(title: "Title" , maxLines: 1, hint: 'title',),
+          SizedBox(height: 20,),
+          CustomTextField(title: "Content", maxLines: 5, hint: "content")
+        ],
+      ),
 
     );
   }
---
+++ lib/Views/Widgets/CustomTextField.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  final String title;
+  final int maxLines;
+  final String hint;
+  const CustomTextField({super.key,required this.title, required this.maxLines, required this.hint});
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    var outlineInputBorder = OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(color: Colors.white, style: BorderStyle.solid));
+    return TextField(
+      maxLines: widget.maxLines,
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+          label: Text(widget.title),
+          hintText: widget.hint,
+          hintStyle: TextStyle(
+            color: Colors.white,
+
+          ),
+
+          enabledBorder: outlineInputBorder,
+          focusedBorder: outlineInputBorder,
+
+          border: outlineInputBorder),
+    );
+  }
+}
