68c3960da8e1843abcc2009e79993d8951bbc4e8
xhambluefox <inforachid.rb@gmail.com>
2023-07-14 23:44:09 +0100

    Init HiveBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'Const/ColorsConst.dart';
 import 'Views/HomePage.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox("Note_box");
   runApp(const MyApp());
 }
 
